92bf649ac3c0b95abd6726b2a27b0ceeed897ceb
EckleyZ <98994306+EckleyZ@users.noreply.github.com>
2022-03-03 16:02:12 -0500

    Update AutoEncoder_Iter1.py
    Spacing changes and update to saving model

--- AutoEncoder_Iter1.py
+++ AutoEncoder_Iter1.py
@@ -30,6 +30,7 @@ tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 #%% Load the Dataset
 P_data = np.load("D:\\Non Windows Stuff\\HPC Research\\Airfoil_Performance_Data.npy")
 
+# split into training and testing sets
 sets = 24
 split = int(P_data.shape[0]/sets)
 P_train = np.zeros([(sets-1)*split, P_data.shape[1], P_data.shape[2], P_data.shape[3]])
@@ -68,24 +69,20 @@ class Autoencoder(Model):
     self.encoder.add(LeakyReLU(alpha=0.2))
     self.encoder.add(BatchNormalization(axis=-1))
     
-    
     # Convolution 2 ---- (None,121,201,8) --> (None,41,34,16)
     self.encoder.add(Conv2D(F[1],(5,9),(3,5),padding='same'))
     self.encoder.add(LeakyReLU(alpha=0.2))
     self.encoder.add(BatchNormalization(axis=-1))
-    
     
     # Convolution 3 ---- (None,41,34,16) --> (None,21,12,32)
     self.encoder.add(Conv2D(F[2],(3,3),(2,2),padding='same'))
     self.encoder.add(LeakyReLU(alpha=0.2))
     self.encoder.add(BatchNormalization(axis=-1))
     
-    
     # Convolution 4 ---- (None,21,12,32) --> (None,10,10,64)
     self.encoder.add(Conv2D(F[3],(3,3),(2,2),padding='valid'))
     self.encoder.add(LeakyReLU(alpha=0.2))
     self.encoder.add(BatchNormalization(axis=-1))
-    
     
     # Prepare output
     self.encoder.add(Flatten())
@@ -100,27 +97,25 @@ class Autoencoder(Model):
     self.decoder.add(Dense(F[3]*outShape))
     self.decoder.add(Reshape([10,10,F[3]]))
     
-    # Deconvolution 2 ---- (None,10,10,64) --> (None,21,12,32)
+    # Deconvolution 1 ---- (None,10,10,64) --> (None,21,12,32)
     self.decoder.add(Conv2DTranspose(F[2],(3,3),(2,2),padding='valid'))
     self.decoder.add(LeakyReLU(alpha=0.2))
     self.decoder.add(BatchNormalization(axis=-1))
     
-    
-    # Deconvolution 3 ---- (None,21,12,32) --> (None,41,34,16)
+    # Deconvolution 2 ---- (None,21,12,32) --> (None,41,34,16)
     self.decoder.add(Conv2DTranspose(F[1],(3,3),(2,2),padding='same',output_padding=(0,0)))
     self.decoder.add(LeakyReLU(alpha=0.2))
     self.decoder.add(BatchNormalization(axis=-1))
     
-    # Deconvolution 4 ---- (None,41,34,16) --> (None,121,201,8)
+    # Deconvolution 3 ---- (None,41,34,16) --> (None,121,201,8)
     self.decoder.add(Conv2DTranspose(F[0],(5,9),(3,5),padding='same',output_padding=(0,0)))
     self.decoder.add(Reshape([121,201,F[0]]))
     self.decoder.add(LeakyReLU(alpha=0.2))
     self.decoder.add(BatchNormalization(axis=-1))
     
-    # Deconvolution 5 ---- (None,121,100,8) --> (None,121,201,10,1)
+    # Deconvolution 4 ---- (None,121,100,8) --> (None,121,201,10,1)
     self.decoder.add(Reshape([121,201,1,F[0]]))
     self.decoder.add(Conv3DTranspose(1,(1,1,10),(1,1,1),activation='sigmoid',padding='valid'))
-    
 
   def call(self, x):
     encoded = self.encoder(x)
@@ -136,7 +131,7 @@ autoencoder.encoder.summary()
 autoencoder.decoder.summary()
 
 
-#%% Train the model with x_test as input and the target output
+#%% Train the model
 
 # input P_train should receive P_train back
 autoencoder.fit(P_train, P_train,
@@ -146,15 +141,13 @@ autoencoder.fit(P_train, P_train,
 
 
 #%% Test the model by encoding and decoding the test data
-
 encoded_imgs = autoencoder.encoder(P_test).numpy()
 decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
 
 print(decoded_imgs.shape)
 
 
-#%% Plot them (not really images but they should look similar) (only used CL, CD, and CM as R, G, and B
-
+#%% Plot the decoded data (not really images but they should look similar) (only used CL, CD, and CM as R, G, and B
 n = 66
 plt.figure(figsize=(64, 32))
 r = 11
@@ -177,10 +170,10 @@ for i in range(n):
   ax.get_yaxis().set_visible(False)
 plt.show()
 
+
 #%% Save model 
 model_Name = 'Models\\AEmodel_recent\\Model'    # Change name depending on model
 
-#autoencoder.encoder.save_weights(model_Name)
 autoencoder.save(model_Name)
       
     
